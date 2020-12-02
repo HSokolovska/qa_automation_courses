f03c55e926a6837852cb7f48f2dfcef278028112
Halyna Sokolovska <halynasokolovska@Halynas-MacBook-Pro.local>
2020-12-01 19:28:43 -0500

    Home Task

---
+++ src/main/java/MyFirstProgram.java
@@ -0,0 +1,33 @@
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        int x = 5, y = 6;
+        final int z = 5;
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        // -128 to 127 and w 1 byte
+        byte bt = 100;
+
+        //-32768 to 32767
+        short sht = 32677;
+
+        // -2147483648 to 2147483667
+        int integer = 22222222;
+
+        //-9223372036854775888 to 9223372036854775887
+        long lng = 22724775999999987L;
+
+        // 0.111111111111111
+        double dbl = 0.8;
+
+        float flt = 0.8f;
+
+        // 0 - 65535
+        char chr = '@';
+
+
+        System.out.println();
+
+    }
+}
---
+++ src/main/java/StringBuilderTypes.java
@@ -0,0 +1,34 @@
+public class StringBuilderTypes {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Warm");
+
+        sb.append(" Greetings");
+        System.out.println(sb);
+
+        sb.deleteCharAt(4);
+        System.out.println(sb);
+
+        sb.delete(3, 6);
+        System.out.println(sb);
+
+        sb.insert(5, "\u263A" + "Hope it looks right" + "\u263A");
+        System.out.println(sb);
+
+        sb.replace(1, 30, "ing");
+        System.out.println(sb);
+
+        sb.reverse();
+        System.out.println(sb);
+
+        System.out.println(sb.substring(3));
+
+        // Or
+        // String a = sb.substring(3);
+        //System.out.println(a);//
+
+        System.out.println(sb.substring(1, 3));
+
+
+    }
+}
+
